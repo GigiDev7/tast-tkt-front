a0515f4285025bcf87e1dd3f7f029ecdce11c50b
GigiDev7 <gigivadachkoria@gmail.com>
2023-03-10 21:08:50 +0400

    bug fixes

--- src/pages/SingleEvent.tsx
+++ src/pages/SingleEvent.tsx
@@ -1,7 +1,24 @@
 import React from "react";
 
 const SingleEvent = () => {
-  return <div>SingleEvent</div>;
+  const tickets = [
+    {
+      date: "03-10-2023",
+      status: "Verified",
+      customer: "Test Test",
+      price: 40,
+    },
+  ];
+  return (
+    <div className="flex flex-col items-center">
+      <h2 className="text-xl font-semibold">Purchase Ticket</h2>
+      <div>
+        {tickets.map((ticket) => (
+          <p>{ticket.price}</p>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default SingleEvent;
